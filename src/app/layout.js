a3e652165f2ba9a781e94184222f23b739084d07
Sujas Aggarwal <ce1221047@iitd.ac.in>
2024-02-10 06:42:52 +0530

    Changed Pos

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,9 +18,9 @@ export default function RootLayout({ children }) {
         
         {children}
       <Footer/>
+      <SpeedInsights/>
       </body>
       </ClerkProvider>
-      <SpeedInsights/>
 
     </html>
   );
