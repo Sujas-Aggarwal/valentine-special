8c0d935f9f7088395353a4a3ba4091f30d1de24c
Sujas Aggarwal <ce1221047@iitd.ac.in>
2024-02-10 19:44:12 +0530

    chd

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ function Home() {
       <h1 className='font-bold  text-2xl sm:text-3xl'>This <span className='text-4xl '>Valentines</span>, <br></br>Express Your Love Beautifully</h1>
       <div className='flex justify-start w-full items-center gap-2 flex-wrap'>
  
-      <Link href={"/account"} className='bg-white min-w-[60%] text-center hover:min-w-[80%] py-2 sm:px-6 sm:py-3 duration-150 transition-all text-[#f63555] rounded-lg font-bold'>Get Started</Link>
+      <Link href={"/account"} className='bg-white min-w-[60%] text-center hover:scale-[1.04] py-2 sm:px-6 sm:py-3 duration-150 transition-all text-[#f63555] rounded-lg font-bold'>Get Started</Link>
       </div>
       </div>
 
