6c3f99146b7ff0acb394631c4ccc7674075bfc9b
Sujas Aggarwal <ce1221047@iitd.ac.in>
2024-02-10 06:38:37 +0530

    changed single quote to double

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Footer from "@/components/footer";
-const font = Poppins({ subsets: ["latin"], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']});
+const font = Poppins({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]});
 
 export const metadata = {
   title: "Create Next App",
